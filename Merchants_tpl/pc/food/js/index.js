228e0e756149519eb8d16b987e424bb6668ad1d5
814171842@qq.com <814171842@qq.com>
2016-12-10 17:23:59 +0800

    '商品分类追模板追加'

--- Merchants_tpl/pc/food/js/index.js
+++ Merchants_tpl/pc/food/js/index.js
@@ -3,21 +3,6 @@ $(function(){
 // $('.s2NaN').removeClass('s2Nav');
 // $('.s2NaN').addClass('s2TopM');
 
-	$('.s2Top >ul li').bind('mouseover',function(){
-
-
-			$('.s2Top > ul li').each(function(){				
-				$($(this).attr('i')).css('display','none');				
-				$($(this).attr('i')).hide();	
-				$(this).removeClass('s2Nav');
-				$(this).addClass('s2TopM');			
-
-			})
-
-		$(this).removeClass('s2TopM');
-		$(this).addClass('s2Nav');
-		$($(this).attr('i')).show();
-		});
 
 
 
